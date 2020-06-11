7a6efa93665ce9331ef6016de7031a6c18b0b707
Atilio Gabriello <gabrielloatilio@gmail.com>
2020-06-11 15:42:32 -0300

    Update

--- site/Resources/Js/Script.js
+++ site/Resources/Js/Script.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
-    $('.js--section-features').waypoints(function(direction){
+    $('.js--section-features').waypoint(function(direction){
         if (direction=="down"){
             $('nav').addClass('sticky');
         }else{
             $('nav').removeClass('sticky');
         }
+    },{
+      offset:'25%'
     });
 });
 
